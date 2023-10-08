26c1ce8f1f9d199998f9dc0089fabad4d780e3ab
Craig McClanahan <craigmcc@gmail.com>
2023-10-07 19:06:26 -0700

    50:00 Set up Clerk sign in and sign up pages.

---
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,7 @@
+// app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+    return <SignIn />;
+}
---
+++ src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+// app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+    return <SignUp />;
+}
