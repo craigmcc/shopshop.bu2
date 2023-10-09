dc2215aeef4a1332332101ec6c766007fda37392
Craig McClanahan <craigmcc@gmail.com>
2023-10-08 18:33:43 -0700

    Delete prisma directory so we can start over.

--- prisma/migrations/20231009003821_add_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "lists_profileId_idx" ON "lists"("profileId");
-
--- CreateIndex
-CREATE INDEX "members_listId_idx" ON "members"("listId");
-
--- CreateIndex
-CREATE INDEX "members_profileId_idx" ON "members"("profileId");
